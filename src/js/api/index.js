dc109caa8e08bb4f900eaa49c4c598c1d9d42d42
Khrystyna Skvarok <christina.skvarok@gmail.com>
2016-11-22 11:41:47 +0200

    Added more questions to quiz

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -2,7 +2,23 @@ const questions = [
   ["I appologize", "for", "being late"],
   ["I asked", "for", "some coffee"],
   ["I cared", "for", "him when he was sick"],
-  ["Parents protect their children", "from", "problems"]
+  ["Parents protect their children", "from", "problems"],
+  ["She suffered", "from", "hay fever"],
+  ["I believe", "in", "you!"],
+  ["I saved a kitten", "from", "fire!"],
+  ["I don't approve", "of", "this behavior"],
+  ["This cake smells", "of", "strawberries"],
+  ["I need to concentrate", "on", "my homework"],
+  ["I agree", "with", "you"],
+  ["We agreed", "on", "almost everything"],
+  ["I introduced him", "to", "my parents"],
+  ["I advised my boyfriend", "to", "buy a car"],
+  ["It depends", "on", "the weather"],
+  ["His parents punished him", "for", "smoking"],
+  ["I applied", "for", "a new job"],
+  ["I will vote", "for", "you!"],
+  ["Don't worry", "about", "anything!"]
+  ["Do you like to listen", "to", "the fire crackling?"]
 ];
 
 export const getQuestions = () => questions;
